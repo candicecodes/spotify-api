0c8219a8439e39a9958828f36bcebd2d326f30bd
Candice Mayes <ca.mayes239@gmail.com>
2018-02-10 23:59:16 -0500

    worked on app component

---
+++ dev/scripts/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Gr<span className="highlight">ooo</span>vin'</h1>
+                <div className="App">
+                    {/* Add a SearchBar component */}
+                    <div className="App-playlist">
+                        {/*  Add a SearchResults component */}
+                        {/* Add a Playlist component */}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
