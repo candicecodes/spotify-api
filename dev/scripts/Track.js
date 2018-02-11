460f2ae91c03225c4d360c1b364fadfca18aac76
Candice Mayes <ca.mayes239@gmail.com>
2018-02-11 14:55:34 -0500

    created renderAction method

--- dev/scripts/Track.js
+++ dev/scripts/Track.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 class Track extends React.Component {
+    constructor(props) {
+        super(props);
+        this.renderAction = this.renderAction.bind(this);
+    }
     render() {
         return (
             <div class="Track">
                 <div class="Track-information">
                     <h3>{/* track name will go here */}</h3>
                     <p>{/* track artist will go here */} | {/* track album will go here */}</p>
+                    {this.renderAction()}
                 </div>
-                <a class="Track-action">{/* + or - will go here */}</a>
+                
+            </div>
+        );
+    }
+    renderAction() {
+        return (
+            <div>
+                {/* <a class="Track-action">+ or - will go here</a> */}
+                <a class="Track-action">{isRemoval ? '-' : '+'}</a>
             </div>
         );
     }
