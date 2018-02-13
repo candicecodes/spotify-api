f684c53d3381dc0ad6e8355ee8f7ce12087c0aa0
Candice Mayes <ca.mayes239@gmail.com>
2018-02-12 22:19:04 -0500

    added three hard-coded tracks for now

--- dev/scripts/App.js
+++ dev/scripts/App.js
@@ -4,6 +4,12 @@ import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchResults =[{ name1, artist1, album1 }, { name2, artist2, album2 }, { name3, artist3, album3}]
+        };
+    }
     render() {
         return (
             <div>
